bc5fa31055c0f59a1c4fffdc85926266cd809eb3
cdelano266 <kim.thunderbird@gmail.com>
2015-11-17 12:33:59 -0500

    started outputting from main.c

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(){
+void loop() {
+  int status;
+  char input[256];
+  char cwd[1024];
+  
+  status = 1;
+
+  while(status){
+    getcwd(cwd, sizeof(cwd));
+    printf("day-sh:%s$ ", cwd);
+    fgets(input, 256, stdin);
+    input[strlen(input) - 1] = '\0';
+    printf("%s\n", input);
+  }
+}
+
+int main() {
+  loop();
+    
   return 0;
 }
