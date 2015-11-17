a7d8ea39dc5afa3bb7ed07666df672243dce80f6
cdelano266 <kim.thunderbird@gmail.com>
2015-11-17 12:08:31 -0500

    Edited Makefile and started main.c

--- Makefile
+++ Makefile
@@ -1,1 +1,9 @@
+all: main.c
+	gcc main.c -o main.out
 
+clean:
+	rm *.out
+	rm *~
+
+run:
+	./main.out
--- main.c
+++ main.c
@@ -2,3 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
+int main(){
+  return 0;
+}
