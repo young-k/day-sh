3ef45c168a8e9226c31b99a37bfa6cdfcf97bf9b
cdelano266 <kim.thunderbird@gmail.com>
2015-11-17 23:21:38 -0500

    added comments for main.c , removed main.h

--- main.c
+++ main.c
@@ -3,28 +3,34 @@
 #include <string.h>
 #include <unistd.h>
 
+/*
+ * Loop:
+ *   The basic while loop of the function. 
+ *   while(True) is not used so that CTRL+C can be handled by the program.
+ *   Deals with parsing input and printing the current working directory.
+ */
 void loop() {
-  int status;
-  char input[256];
-  char cwd[256];
-  char *home;
+  int status;			/* boolean; whether to exit loop or not */
+  char input[256];		/* input from stdin */
+  char cwd[256];		/* abbrev. for current working directory */
+  char *home;			/* PATH of '~' */
   
-  status = 1;
+  status = 1; 			
 
   home = getenv("HOME");
   
   while(status) {
     getcwd(cwd, sizeof(cwd));
 
-    if(strstr(cwd,home) != NULL) {
-      char pwd[256] = "~";
+    if(strstr(cwd,home) != NULL) { /* if the PATH of '~' is in the cwd */
+      char pwd[256] = "~";	   /* replace PATH with '~' for brevity */
       strcat(pwd, cwd + strlen(home));
       printf("day-sh:%s$ ", pwd);
-    } else {
+    } else {			   /* else print normal cwd */
       printf("day-sh:%s$ ",cwd);
     }
     
-    fgets(input, 256, stdin);
+    fgets(input, 256, stdin);	/* read input and add terminating null */
     input[strlen(input) - 1] = '\0';
     
     printf("%s\n", input);
@@ -32,7 +38,6 @@ void loop() {
 }
 
 int main() {
-  loop();
-    
+  loop();    
   return 0;
 }
