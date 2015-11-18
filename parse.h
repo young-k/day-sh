9a80f3ea008ee5e1896c75e0a23788f348c1ca87
Aaron Wang <az0977776@yahoo.com>
2015-11-18 06:28:14 -0500

    parse header xD

--- parse.h
+++ parse.h
@@ -1,7 +1,7 @@
 
 /*
  * Function Description:
- *   parse_commands takes in a string of a single command with command line
+ *   parse_commands() takes in a string of a single command with command line
  *   arguments and puts it in a char * array with the first element being the
  *   command and the following elements are the arguments. The last element
  *   of the array is NULL
@@ -17,17 +17,43 @@ char** parse_commands (char *) {}
 
 /*
  * Function Description:
- *   parse_inputs takes in the raw input string from stdin and puts each
- *   command seperated by a semicolon in to an array of commands
+ *   parse_inputs() takes in the raw input string from stdin and puts each
+ *   command separated by a semicolon in to an array of commands
  *
  * Arguments:
- *   char* of the raw input
+ *   char * of the raw input
  *
  * Return:
- *   char** of commands
+ *   char ** of commands
  *
  */
 char** parse_inputs (char *) {}
 
+/*
+ * Function Description:
+ *   count_tokens() returns the number of times the character delimiter appears  
+ *   in the string line
+ *
+ * Arguments:
+ *   char * string 
+ *   char * delimiter
+ *
+ * Return:
+ *   number of tokens in line separated by delim
+ *
+ */
+int count_tokens(char *, char*) {}
 
-
+/*
+ * Function Description:
+ *   trim() removes leading and trailing whitespace of the string
+ *
+ * Arguments:
+ *   char *string
+ *
+ * Returns:
+ *   string without leading and trailing whitespace
+ *
+ */
+char *trim(char *) {}
+ 
