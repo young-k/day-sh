a8cb249d64e40fad129d10295d19a1164cc736fb
Derrick Lui <derricklui1@gmail.com>
2015-11-18 11:52:24 -0500

    updated parse_input, still working on it

--- parse.c
+++ parse.c
@@ -9,20 +9,30 @@
 
 
 char ** parse_input(char *input) {
-  char temp[1][1];
-  return temp;
-}
+	char ** output[count_tokens(input,";")];
+	
+	//Takes input and parses it
+	input[strlen(input) - 1] = '\0';
+	char *command;
+	command = input;
+	command = strsep(&command, "\n");
+	
+	while (command
 
-char ** parse_commands(char *commands){
-  char *argray[15];
-  int counter = 0;
-  while (commands) {
-    argray[counter] = strsep(&commands, " ");
-    counter++;
-  }
-  char temp[1][1];
-  return temp;
-}
+	
+				 execlp(
+								}
+
+				 char ** parse_commands(char *commands){
+					 char *argray[15];
+					 int counter = 0;
+					 while (commands) {
+						 argray[counter] = strsep(&commands, " ");
+						 counter++;
+					 }
+					 char temp[1][1];
+					 return temp;
+				 }
 
 
   
