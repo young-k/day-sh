4c7ced9d5fa7c88cce9642c1c7a6941363e643ef
Derrick Lui <derricklui1@gmail.com>
2015-11-20 10:11:33 -0500

    completely finished trim

--- parse.c
+++ parse.c
@@ -5,40 +5,40 @@
 #include <ctype.h>
 
 
-char ** parse_commands(char *commands) {
-  char **argray;
-  //first index would be count_tokens(commands, " ")
-  int counter = 0;
-  char **argray = abc;
+/* char ** parse_commands(char *commands) {
+	 char **argray;
+	 //first index would be count_tokens(commands, " ")
+	 int counter = 0;
+	 char **argray = abc;
   
-  while (commands) {
-    argray[counter] = strsep(&commands, " ");
-    counter++;
-  }
-  argray[counter] = NULL;
-  return argray;
-}
+	 while (commands) {
+	 argray[counter] = strsep(&commands, " ");
+	 counter++;
+	 }
+	 argray[counter] = NULL;
+	 return argray;
+	 }
 
-char ** parse_input(char *input) {
-  char **output;
-  //first index is count_tokens(input, ";")
-  int counter = 0;
+	 /* char ** parse_input(char *input) { */
+/*   char **output; */
+/*   //first index is count_tokens(input, ";") */
+/*   int counter = 0; */
   
-  input = strsep(&input, "\n");
-  input[strlen(input) - 1] = '\0';
-  char *command;
-  char *commandarray[20];
+/*   input = strsep(&input, "\n"); */
+/*   input[strlen(input) - 1] = '\0'; */
+/*   char *command; */
+/*   char *commandarray[20]; */
   
-  command = input;
-  command = strsep(&command, "\n");
+/*   command = input; */
+/*   command = strsep(&command, "\n"); */
 	
-  while (command){
-    argray[counter] = strsep(&command, ";");
-    counter+=1;
-  }
-  argray[counter] = NULL;
-  return commandarray;
-}
+/*   while (command){ */
+/*     argray[counter] = strsep(&command, ";"); */
+/*     counter+=1; */
+/*   } */
+/*   argray[counter] = NULL; */
+/*   return commandarray; */
+/* } */
 
 int count_tokens(char *s1, char *delim) {
   int counter = 0;
@@ -49,15 +49,17 @@ int count_tokens(char *s1, char *delim) {
   return counter;
 }
 
-char* trim(char *s1) {
-  char *pointer;
-	if (!s1)
-		return NULL;
-	if (!*s1)
-		return s1;
-	for (pointer = s1 + strlen(s1) - 1; (pointer >= s1) && isspace(*pointer); pointer--);
-	pointer[1] = '\0';
-	return s1;
+char* trim(char *str) {
+	char *end;
+	while (isspace(*str)) str++;
+	if(*str == 0)
+		return str;
+	end = str + strlen(str) -1;
+	while(end>str && isspace(*end)) end--;
+
+	*(end+1) = 0;
+
+	return str;
  
 }
 
@@ -68,13 +70,16 @@ int main() {
 	char asd[] = "this is a sentence";
 	printf("%d\n", count_tokens(asd," "));
 
+	char cool[] = "     trim     test     ";
+	printf("%s\n", trim(cool));
   
-	char test[] = "ls -a -l";
-	char *test1 = test;
-	char **test2 = parse_commands(test1);
-	int i;
-	for (i = 0; i < 14;i ++){
-		printf("%s\n", test2[i]);
-	}
-	return 0;
+	/* char test[] = "ls -a -l"; */
+	/* char *test1 = test; */
+	/* char **test2 = parse_commands(test1); */
+	/* int i; */
+	/* for (i = 0; i < 14;i ++){ */
+	/* 	printf("%s\n", test2[i]); */
+	/* } */
+	/* return 0; */
+
 }
