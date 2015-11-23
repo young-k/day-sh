045c5d835facf6789971047f233d0ab21971d9ff
Aaron Wang <az0977776@yahoo.com>
2015-11-22 21:53:22 -0500

    i actually have no idea

--- parse.c
+++ parse.c
@@ -4,41 +4,42 @@
 #include <unistd.h>
 #include <ctype.h>
 
+/*
+char ** parse_commands(char *commands) {
+  char **argray;
+  //first index would be count_tokens(commands, " ")
+  int counter = 0;
+  char **argray = abc;
+  
+  while (commands) {
+    argray[counter] = strsep(&commands, " ");
+    counter++;
+  }
+  argray[counter] = NULL;
+  return argray;
+}
 
-/* char ** parse_commands(char *commands) {
-	 char **argray;
-	 //first index would be count_tokens(commands, " ")
-	 int counter = 0;
-	 char **argray = abc;
+char ** parse_input(char *input) {
+  char **output;
+  //first index is count_tokens(input, ";")
+  int counter = 0;
   
-	 while (commands) {
-	 argray[counter] = strsep(&commands, " ");
-	 counter++;
-	 }
-	 argray[counter] = NULL;
-	 return argray;
-	 }
-
-	 /* char ** parse_input(char *input) { */
-/*   char **output; */
-/*   //first index is count_tokens(input, ";") */
-/*   int counter = 0; */
+  input = strsep(&input, "\n");
+  input[strlen(input) - 1] = '\0';
+  char *command;
+  char *commandarray[20];
   
-/*   input = strsep(&input, "\n"); */
-/*   input[strlen(input) - 1] = '\0'; */
-/*   char *command; */
-/*   char *commandarray[20]; */
-  
-/*   command = input; */
-/*   command = strsep(&command, "\n"); */
+  command = input;
+  command = strsep(&command, "\n");
 	
-/*   while (command){ */
-/*     argray[counter] = strsep(&command, ";"); */
-/*     counter+=1; */
-/*   } */
-/*   argray[counter] = NULL; */
-/*   return commandarray; */
-/* } */
+  while (command){
+    argray[counter] = strsep(&command, ";");
+    counter+=1;
+  }
+  argray[counter] = NULL;
+  return commandarray;
+}
+*/
 
 int count_tokens(char *s1, char *delim) {
   int counter = 0;
@@ -62,17 +63,53 @@ char* trim(char *str) {
   return str;
 }
 
+char** generate_array(int a, int b) {
+  printf("step 2");
+  int i;
+  printf("step 3");
+  char **output = (char **)malloc(a * sizeof(char *));
+  printf("p1");
+  for (i = 0; i < a; i++ ) {
+    output[i] = (char *)malloc(b * sizeof(char));
+  }
+  return output;
+}
   
   
 int main() {
-
+  
+  // test for count_token
   char asd[] = "this is a sentence";
   printf("%d\n", count_tokens(asd," "));
   
-  
+  // test for trim
   char abc[] = "       asdfasldkf      qwrwertwe    ";
-  printf("%s\n", trim(abc));
+  printf("[%s]\n", trim(abc));
+
+  printf("end of trim\n");
   
+  // test for generate_array
+  printf("step 1");
+  char **coolio = generate_array(4,3);
+  printf("step 4");
+  int i, j, count = 0;
+
+  printf("test");
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 3; j++) {
+      *(coolio + i * 4 + j) = (char)count;
+      count++;
+    }
+  }
+
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 3; j++) {
+      printf("%s", *(coolio + i * 4 + j));
+    }
+  }
+  
+
+  /* 
   char test[] = "ls -a -l";
   char *test1 = test;
   char **test2 = parse_commands(test1);
@@ -80,5 +117,6 @@ int main() {
   for (i = 0; i < 14;i ++){
     printf("%s\n", test2[i]);
   }
+  */
   return 0;
 }
